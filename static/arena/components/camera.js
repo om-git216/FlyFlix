e6433250185906c16023140e753551ae1743c4da
Omika Wadhwa <56737677+owadhwa@users.noreply.github.com>
2023-03-07 15:58:23 -0500

    new distance

--- static/arena/components/camera.js
+++ static/arena/components/camera.js
@@ -10,7 +10,7 @@ import { RotatingCamera } from './rotating_camera.js';
 
 function createCamera(orientation) {
     const camera = new RotatingCamera(
-        90,                                         // fov. According to measurements with the Fire tablet, this is closest to true angles at a distance of around 35mm between fly and display.
+        50,                                         // fov. According to measurements with the Fire tablet, this is closest to true angles at a distance of around 35mm between fly and display.
         window.innerWidth / window.innerHeight,     // aspect ratio
         0.001,                                       // near clipping
         2,                                           // far clipping
